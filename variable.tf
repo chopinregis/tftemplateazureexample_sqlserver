29353efc74e401846ccd0f226dea4ed426f9f9b1
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-31 14:15:16 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,42 @@
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "administrator_login"{
+  type=string
+}
+variable "administrator_login_password"{
+  type=string
+}
+variable "name"{
+  type=string
+}
+variable "location"{
+  type=string
+}
+variable "mysqlname"{
+  type=string
+}
+variable "resourceversion"{
+  type=string
+}
+variable "minimum_tls_version"{
+  type=string
+}
+variable "login_username"{
+  type=string
+}
+variable "object_id"{
+  type=string
+}
+variable "environment"{
+  type=string
+}
